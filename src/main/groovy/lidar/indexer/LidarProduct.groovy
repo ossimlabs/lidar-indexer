bb280b9e74a5ba8c97e4e171ea5ac474a03db86d
sbortman <scott.bortman@gmail.com>
2020-06-22 11:13:14 -0400

    Added geometry capability to the rest service.  Can now pass a geometry in wkt format as part of the message and it will conert to a geometry object before saving to the database.   Also,  it will convert a geometry to wkt when outputting as json

--- src/main/groovy/lidar/indexer/LidarProduct.groovy
+++ src/main/groovy/lidar/indexer/LidarProduct.groovy
@@ -1,10 +1,15 @@
 package lidar.indexer
 
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import org.locationtech.jts.geom.Polygon
 
 @Entity
 @Table(name = "lidar_products")
@@ -21,5 +26,10 @@ class LidarProduct {
     String keyword
 
     String s3_link = "https://starwars.s3.amazonaws.com/characters/leia.txt"
+
+    @JsonSerialize(using = WktGeometrySerializer)
+    @JsonDeserialize(using = WktGeometryDeserializer)
+    @Column(columnDefinition = 'geometry(Polygon, 4326)')
+    Polygon bbox    
 
 }
