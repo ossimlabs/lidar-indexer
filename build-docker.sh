9884972940cb23be190aec94bddee7199cb50d4d
sbortman <scott.bortman@gmail.com>
2020-06-23 10:17:51 -0400

    added gradle assemble to build-docker.sh.   Changed mapped port from 8080 -> 9999 for local testing

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+gradle assemble
 docker build -t lidar-indexer .
--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it --rm -p 8080:8080 --env PGHOST=host.docker.internal lidar-indexer
+docker run -it --rm -p 9999:8080 --env PGHOST=host.docker.internal lidar-indexer
