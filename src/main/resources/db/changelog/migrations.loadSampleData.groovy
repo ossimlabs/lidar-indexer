c82f3beb2c22ee744b1a16412bcb1b30f4a707d4
melbsurfer <melbsurfer@gmail.com>
2020-06-29 10:47:13 -0400

    Modified sample data

--- src/main/resources/db/changelog/migrations.loadSampleData.groovy
+++ src/main/resources/db/changelog/migrations.loadSampleData.groovy
@@ -9,37 +9,74 @@ databaseChangeLog {
 			[
 				ingest_date: '04/06/2020',
 				bbox: [ min_x: -180, min_y: -90, max_x: 180, max_y: 90, srs: 4326 ],
-				keyword: 'Luke Skywalker',
-				s3_link: 'https://starwars.s3.amazonaws.com/characters/luke.txt'
+				keyword: 'Damsok',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Damsok.txt'
 			],
 			
 			[
 				ingest_date: '01/14/2020',
 				bbox: [ min_x: -180, min_y: 0, max_x: 0, max_y: 90, srs: 4326 ],
-				keyword: 'Obi-Wan Kenobi',
-				s3_link: 'https://starwars.s3.amazonaws.com/characters/obi-wan.txt'
+				keyword: 'Cheoleang',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Cheoleang.txt'
 			],
 			
 			[
 				ingest_date: '11/01/2019',
 				bbox: [ min_x: 0, min_y: 0, max_x: 180, max_y: 90, srs: 4326 ],
-				keyword: 'Kylo Ren',
-				s3_link: 'https://starwars.s3.amazonaws.com/characters/kylo.txt'
+				keyword: 'Siboljur',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Siboljur.txt'
 			],
 			
 			[
 				ingest_date: '02/14/2020',
 				bbox: [ min_x: -180, min_y: -90, max_x: 0, max_y: 0, srs: 4326 ],
-				keyword: 'R2D2',
-				s3_link: 'https://starwars.s3.amazonaws.com/characters/r2d2.txt'
+				keyword: 'Salabang',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Salabang.txt'
 			],
 			
 			[
-				ingest_date: '12/24/2019',
+				ingest_date: '05/10/2017',
 				bbox: [ min_x: -45, min_y: -45, max_x: 45, max_y: 45, srs: 4326 ],
-				keyword: 'BB8',
-				s3_link: 'https://starwars.s3.amazonaws.com/characters/bb8.txt'
+				keyword: 'Tamjung',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Tamjung.txt'
 			],
+
+			[
+				ingest_date: '01/20/2016',
+				bbox: [ min_x: -45, min_y: -45, max_x: 45, max_y: 45, srs: 4326 ],
+				keyword: 'Nabadawan',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Nabadawan.txt'
+			],
+
+			[
+				ingest_date: '04/01/2018',
+				bbox: [ min_x: -45, min_y: -45, max_x: 45, max_y: 45, srs: 4326 ],
+				keyword: 'Okwin',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Okwin.txt'
+			],
+
+			[
+				ingest_date: '12/01/2020',
+				bbox: [ min_x: -45, min_y: -45, max_x: 45, max_y: 45, srs: 4326 ],
+				keyword: 'Thedan',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Thedan.txt'
+			],
+
+			[
+				ingest_date: '07/24/2020',
+				bbox: [ min_x: -45, min_y: -45, max_x: 45, max_y: 45, srs: 4326 ],
+				keyword: 'Cotanila',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Cotanila.txt'
+			],
+
+			[
+				ingest_date: '08/09/2020',
+				bbox: [ min_x: -45, min_y: -45, max_x: 45, max_y: 45, srs: 4326 ],
+				keyword: 'Rodiz',
+				s3_link: 'https://lidar.s3.amazonaws.com/data/Rodiz.txt'
+			],
+
+
 		
 		].each { record ->
 			insert( tableName: 'lidar_products' ) {
