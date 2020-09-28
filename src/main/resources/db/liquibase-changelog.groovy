fa9c2c123040e3c8361575661ae663de04c96f08
melbsurfer <melbsurfer@gmail.com>
2020-09-28 09:47:40 -0400

    Removing the database seed with sample data.

--- src/main/resources/db/liquibase-changelog.groovy
+++ src/main/resources/db/liquibase-changelog.groovy
@@ -3,5 +3,6 @@ package db
 databaseChangeLog {
     include file: 'changelog/migrations.installPostgis.groovy', relativeToChangelogFile: true
     include file: 'changelog/migrations.initialSchema.groovy', relativeToChangelogFile: true
-    include file: 'changelog/migrations.loadSampleData.groovy', relativeToChangelogFile: true
+    // Uncomment the line below to seed the database with test data
+    // include file: 'changelog/migrations.loadSampleData.groovy', relativeToChangelogFile: true
 }
